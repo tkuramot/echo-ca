faf11706e8556fa549f9160414b035960061fcd5
tkuramot <mabdman512@gmail.com>
2024-09-22 03:15:52 +0900

    add user repository

--- go.mod
+++ go.mod
@@ -2,6 +2,8 @@ module github/tkuramot/echo-practice
 
 go 1.22.4
 
-require github.com/oklog/ulid/v2 v2.1.0
-
-require github.com/google/go-cmp v0.6.0 // indirect
+require (
+	github.com/google/go-cmp v0.6.0
+	github.com/oklog/ulid/v2 v2.1.0
+	go.uber.org/mock v0.4.0
+)
---
+++ internal/domain/user/user_repository.go
@@ -0,0 +1,10 @@
+//go:generate mockgen -package $GOPACKAGE -source $GOFILE -destination mock_$GOFILE
+package user
+
+import "context"
+
+type UserRepository interface {
+	Save(ctx context.Context, user *User) error
+	FindByID(ctx context.Context, id string) (*User, error)
+	FindAll(ctx context.Context) ([]*User, error)
+}
