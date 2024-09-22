364ff0c08e471d75f0e3655c9a5e1e014e6c3986
tkuramot <mabdman512@gmail.com>
2024-09-22 21:21:51 +0900

    generate user queries using sqlc

---
+++ internal/infrastructure/mysql/db/query/user.sql
@@ -0,0 +1,35 @@
+-- name: UserFindById :one
+SELECT
+    *
+FROM
+    users
+WHERE
+    id = ?;
+
+-- name: UpsertUser :exec
+INSERT INTO
+    users (
+    id,
+    email,
+    nickname,
+    created_at,
+    updated_at
+)
+VALUES
+    (
+        sqlc.arg(id),
+        sqlc.arg(email),
+        sqlc.arg(nickname),
+        NOW(),
+        NOW()
+    ) ON DUPLICATE KEY
+UPDATE
+    email = sqlc.arg(email),
+    nickname = sqlc.arg(nickname),
+    updated_at = NOW();
+
+-- name: UserFindAll :many
+SELECT
+    *
+FROM
+    users;
