a300384987fc0463dcb6321f96105c323fec5fef
tkuramot <mabdman512@gmail.com>
2024-09-22 23:58:08 +0900

    add main func

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"context"
+
+	"github/tkuramot/echo-practice/internal/config"
+	"github/tkuramot/echo-practice/internal/infrastructure/mysql/db"
+	"github/tkuramot/echo-practice/internal/server"
+)
+
 func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	conf := config.GetConfig()
+	db.NewMainDB(conf.DB)
+	db.NewReadDB(conf.ReadDB)
+
+	server.Run(ctx, conf)
 }
