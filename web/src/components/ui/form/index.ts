3085a2c1fc75ec9183b48aa5f211875a68f1aa34
tkuramot <mabdman512@gmail.com>
2024-10-05 20:26:11 +0900

    add shadcn as deps

---
+++ web/src/components/ui/form/index.ts
@@ -0,0 +1,25 @@
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  useFormField,
+} from "@/components/ui/form/form";
+import { Input } from "@/components/ui/form/input";
+import { Label } from "@/components/ui/form/label";
+
+export {
+  useFormField,
+  Form,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+  FormField,
+  Input,
+  Label,
+};
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,6 +5,11 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite({}), react()],
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
   server: {
     host: "0.0.0.0",
     port: 3000,
